d2150f22c1cb30d52a00245aae52be692da8c25c
egzorcistt <101678054+egzorcistt@users.noreply.github.com>
2022-03-15 21:13:08 -0300

    fix

--- trabalho_metrica/src/App.vue
+++ trabalho_metrica/src/App.vue
@@ -6,7 +6,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import cardComponent from './components/Example.vue
+import cardComponent from './components/Example.vue'
 
 export default {
   name: 'App',
