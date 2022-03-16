eaa6950c76bea58dd23e90a74a996d67accc79dc
egzorcistt <101678054+egzorcistt@users.noreply.github.com>
2022-03-15 21:09:08 -0300

    new commit

---
+++ trabalho_metrica/src/components/Example.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card">
+  <img alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>John Doe</b></h4>
+    <p>Architect & Engineer</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Example',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+</style>
